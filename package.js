e55525c22b09b3e2eaa5147b6a22d32ca9e5928d
infouniverseforloop <info.universefor.loop@gmail.com>
2025-11-03 22:20:13 +0600

    Create package.json

---
+++ package.js
@@ -0,0 +1,16 @@
+{
+  "name": "binary-sniper-god-final",
+  "version": "1.0.0",
+  "private": true,
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "axios": "^1.4.0",
+    "dotenv": "^16.0.3",
+    "express": "^4.18.2",
+    "node-fetch": "^2.6.7",
+    "uuid": "^9.0.0",
+    "ws": "^8.13.0"
+  }
+    }
